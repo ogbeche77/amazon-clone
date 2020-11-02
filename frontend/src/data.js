663f3d278714c40428c73dd81a9f22e7067390f9
ogbeche77 <ogbeche77@yahoo.com>
2020-11-02 15:36:17 +0100

    initial

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Designer Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Quality Product'
+        },
+        {
+            _id: '2',
+            name: 'Slim Kotki Shirt',
+            category: 'Shirts',
+            image: './images/p2.jpg',
+            price: 110,
+            brand: 'Versace',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'Quality Product'
+        },
+        {
+            _id: '3',
+            name: 'Hogan Designer Shirt',
+            category: 'Shirts',
+            image: './images/p3.jpg',
+            price: 90,
+            brand: 'Hogan',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'Quality Product'
+        },
+        {
+            _id: '4',
+            name: 'Kalax Pants',
+            category: 'Pants',
+            image: './images/p4.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            description: 'Quality Product'
+        },
+        {
+            _id: '5',
+            name: 'Sulum Pants',
+            category: 'Pants',
+            image: './images/p5.jpg',
+            price: 75,
+            brand: 'Sulum',
+            rating: 4.5,
+            numReviews: 18,
+            description: 'Quality Product'
+        },
+        {
+            _id: '6',
+            name: 'Daidos Pant',
+            category: 'Pants',
+            image: './images/p6.jpg',
+            price: 115,
+            brand: 'Daidos',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Quality Product'
+        },
+    ],
+};
+export default data;
