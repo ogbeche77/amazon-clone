ab6372992f6727095d3136c1388827f55b6c9243
ogbeche77 <ogbeche77@yahoo.com>
2020-11-03 09:39:55 +0100

    Ratings & review components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import data from './data';
+import Product from './components/Product';
 function App() {
   return (
     <div className="grid-container">
@@ -16,25 +17,8 @@ function App() {
         <div className="row center">
           {
             data.products.map(product => (
+              <Product key={product._id} product={product}></Product>
 
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img className="medium" src={product.image} alt={product.name} />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star-o"></i></span>
-                  </div>
-                  <div className="price">€{product.price}</div>
-                </div>
-              </div>
             ))
           }
 
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Ratings from './Ratings';
+
+export default function Product(props) {
+    const { product } = props;
+    return (
+        <div key={product._id} className="card">
+            <a href={`/product/${product._id}`}>
+                <img className="medium" src={product.image} alt={product.name} />
+            </a>
+            <div className="card-body">
+                <a href={`/product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                </a>
+                <Ratings rating={product.rating}
+                    numReviews={product.numReviews}
+                ></Ratings>
+                <div className="price">€{product.price}</div>
+            </div>
+        </div>
+    )
+}
