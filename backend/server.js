d4f76b95085e24db9dcc12d403a2d6e63565a773
ogbeche77 <ogbeche77@yahoo.com>
2020-11-04 10:22:59 +0100

    Backend server initiated

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+//const express = require("express");
+import data from './data.js';
+
+const app = express();
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+})
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+});
+const port = process.env.PORT || 5050
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+})
