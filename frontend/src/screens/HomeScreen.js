60ffa462a524d898e8f6c6320227dacab27355e2
ogbeche77 <ogbeche77@yahoo.com>
2020-11-05 18:41:31 +0100

    Redux store added to Home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Product from '../components/Product';
-import axios from 'axios';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
-    const [products, setProducts] = useState([]);
-    //Show Loading
-    const [loading, setLoading] = useState(false)
-    //Show Error
-    const [error, setError] = useState(false)
+    // const [products, setProducts] = useState([]);
+    // //Show Loading
+    // const [loading, setLoading] = useState(false)
+    // //Show Error
+    // const [error, setError] = useState(false)
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList);
+    const { loading, error, products } = productList;
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                const { data } = await axios.get('/api/products'); //array of data in backend transfers to frontend
-                setLoading(false);
-                setProducts(data);
-            } catch (err) {
-                setError(err.message);
-                setLoading(false);
-            }
+        dispatch(listProducts());
+        // const fetchData = async () => {
+        //     try {
+        //         setLoading(true);
+        //         const { data } = await axios.get('/api/products'); //array of data in backend transfers to frontend
+        //         setLoading(false);
+        //         setProducts(data);
+        //     } catch (err) {
+        //         setError(err.message);
+        //         setLoading(false);
+        //     }
 
-        };
-        fetchData();
+        // };
+        // fetchData();
     }, [])
 
     return (
