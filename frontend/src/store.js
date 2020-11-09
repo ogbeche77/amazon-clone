85e133aa60893184a3bc17acad7fe55b19509866
ogbeche77 <ogbeche77@yahoo.com>
2020-11-09 12:16:48 +0100

    Redux added to Cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 //const { default: data } = require("./data");
 
@@ -7,6 +8,7 @@ const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;//Add redux to browser
